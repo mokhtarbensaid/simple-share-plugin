e7f7f86477e9be6a77d5cf44617e5ed5184e6558
Mokhtar Bensaid <38160000+mokhtarbensaid@users.noreply.github.com>
2021-12-24 16:32:37 +0100

    Update mb-share.php

--- mb-share.php
+++ mb-share.php
@@ -14,117 +14,124 @@
  */
 defined( 'ABSPATH' ) or die('You dont access directly' );
 
-//Enqueue styles 
-add_action( 'wp_enqueue_scripts', 'mb_share_styles' );
-function mb_share_styles(){
-    wp_enqueue_style( 'fontawesome-mbs', 'https://pro.fontawesome.com/releases/v5.10.0/css/all.css');
-	wp_enqueue_style( 'style-mbs', plugins_url('assets/css/style.css', __FILE__));
+//Enqueue styles
+if (!function_exists( 'mbs_share_styles' )) {
+add_action( 'wp_enqueue_scripts', 'mbs_share_styles' );
+	function mbs_share_styles(){
+	    wp_enqueue_style( 'mbs-fontawesome', 'https://pro.fontawesome.com/releases/v5.15.0/css/all.css');
+		wp_enqueue_style( 'mbs-style', plugins_url('assets/css/style.css', __FILE__));
+	}
 }
 
-// Add to menu
-add_action( 'admin_menu', 'mb_share_menu' );
-function mb_share_menu(){
+// Add submenu for show plugin settings
+if (!function_exists( 'mbs_share_menu' )) {
 
-	add_submenu_page( 'options-general.php', 'MB Share', 'MB Share', 'manage_options', 'mb-share', 'mb_share_function' );
+	add_action( 'admin_menu', 'mbs_share_menu' );
+	function mbs_share_menu(){
+		add_submenu_page( 'options-general.php', esc_html__( 'MB Share', 'mb-share' ), esc_html__( 'MB Share', 'mb-share' ), 'manage_options', 'mb-share', 'mbs_share_function' );
+	}
 }
 
 // display the button in the frontend
-add_filter('the_content', 'mb_share_buttons',0);
-function mb_share_buttons($content){
-	if (is_single()) { 
-		global $post;
-	    $url = get_permalink($post->ID);
-	    $url = esc_url($url);
+if (!function_exists( 'mbs_share_buttons' )) {
 
-		$content .= '<ul class="mb-share-links">';
-		if (get_option('email_share')==1) {
-			$content.= '<li><a class="mb-share-email" target="_blank" href="mailto:info@example.com?&subject=&cc=&bcc=&body='. $url .'"><i class="fa fa-envelope"></i></a></li>';
-		}
-		if (get_option('facebook_share')==1) {
-			$content.= '<li><a class="mb-share-facebook" target="_blank" href="http://www.facebook.com/sharer.php?u=' . $url . '"><i class="fab fa-facebook-f"></i></a></li>';
-		}
-		if (get_option('twitter_share')==1) {
-			$content.= '<li><a class="mb-share-twitter" target="_blank" href="https://twitter.com/share?url=' . $url . '"><i class="fab fa-twitter"></i></a></li>';
-		}
-		if (get_option('instagram_share')==1) {
-			$content.= '<li><a class="mb-share-instagram" target="_blank" href="https://instagram.com/send?text='. $url .'"><i class="fab fa-instagram"></i></a></li>';
-		}
-				
-		if (get_option('linkedin_share')==1) {
-			$content.= '<li><a class="mb-share-linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?url=' . $url . '"><i class="fab fa-linkedin-in"></i></a></li>';
-		}	
-		$content.='</ul>';
+	add_filter( 'the_content', 'mbs_share_buttons',0 );
+	function mbs_share_buttons( $content ){
+		if ( is_single() ) { 
+			global $post;
+		    $mbs_url = get_permalink( $post->ID );
+		    $mbs_url = esc_url( $url );
 
-		return $content;
+			$content .= '<ul class="mbs-links">';
+			if ( get_option( 'mbs_email_share') == 1 ) {
+				$content.= '<li><a class="mbs-email" target="_blank" href="mailto:%7Bemail_address%7D?subject=&body='. $mbs_url .'"><i class="fa fa-envelope"></i></a></li>';
+			}
+			if ( get_option( 'mbs_facebook_share' ) == 1 ) {
+				$content.= '<li><a class="mbs-facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' . $mbs_url . '"><i class="fab fa-facebook-f"></i></a></li>';
+			}
+			if ( get_option( 'mbs_twitter_share' ) == 1 ) {
+				$content.= '<li><a class="mbs-twitter" target="_blank" href="https://twitter.com/intent/tweet?url=' . $mbs_url . '"><i class="fab fa-twitter"></i></a></li>';
+			}
+			if ( get_option( 'mbs_pinterest_share' ) == 1 ) {
+				$content.= '<li><a class="mbs-pinterest" target="_blank" href="http://pinterest.com/pin/create/button/?url='. $mbs_url .'"><i class="fab fa-pinterest"></i></a></li>';
+			}
+					
+			if ( get_option( 'mbs_linkedin_share') == 1 ) {
+				$content.= '<li><a class="mbs-linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=' . $mbs_url . '"><i class="fab fa-linkedin-in"></i></a></li>';
+			}	
+			$content.='</ul>';
+
+			return $content;
+		}
 	}
 }
 
 
-function mb_share_function(){
+if ( !function_exists( 'mbs_share_function' ) ) {
+
+	function mbs_share_function(){
+	    ?>
+	        <div class="wrap">
+		        <h1><?php echo esc_html__( 'Social Media share:', 'mb-share' ) ?></h1>
+		        <form method="post" action="options.php">
+		            <?php
+		                settings_fields( 'mbs_section' );
+		                do_settings_sections( 'mbs_social_share_links' );
+		                submit_button(); 
+		            ?>          
+		        </form>
+	        </div>
+	    <?php
+	}
+}
+
+if ( !function_exists( 'mbs_display_social_links_fields' ) ) {
+
+	add_action( 'admin_init', 'mbs_display_social_links_fields' );
+	function mbs_display_social_links_fields(){
+
+	    add_settings_section( 'mbs_section', esc_html__( 'Social Media share:', 'mb-share' ), null, 'mbs_social_share_links' );
+	    add_settings_field( 'mbs_email_share', esc_html__( 'Email:', 'mb-share' ), 'mbs_display_email_field', 'mbs_social_share_links', 'mbs_section');
+	    add_settings_field( 'mbs_facebook_share', esc_html__( 'Facebook:', 'mb-share' ), 'mbs_display_facebook_field', 'mbs_social_share_links', 'mbs_section');   
+	    add_settings_field( 'mbs_twitter_share', esc_html__( 'Twitter:', 'mb-share' ), 'mbs_display_twitter_field', 'mbs_social_share_links', 'mbs_section');
+	    add_settings_field( 'mbs_pinterest_share', esc_html__( 'Pinterest:', 'mb-share' ), 'mbs_display_pinterest_field', 'mbs_social_share_links', 'mbs_section');
+	    add_settings_field( 'mbs_linkedin_share', esc_html__( 'Linkedin:', 'mb-share' ), 'mbs_display_linkedin_field', 'mbs_social_share_links', 'mbs_section');
+	    
+	    register_setting( 'mbs_section', 'mbs_email_share' );
+	    register_setting( 'mbs_section', 'mbs_facebook_share' );
+	    register_setting( 'mbs_section', 'mbs_twitter_share' );
+	    register_setting( 'mbs_section', 'mbs_pinterest_share' );
+	    register_setting( 'mbs_section', 'mbs_linkedin_share' );
+	}
+}
+
+//Adding Social sharing checkboxes to dashboard
+function mbs_display_email_field(){
     ?>
-        <div class="wrap">
-	        <h1>Social Media share:</h1>
-	        <span>
-	            <?php settings_errors(); ?>
-	        </span>
-	        <form method="post" action="options.php">
-	            <?php
-	                settings_fields("section");
-	                do_settings_sections("social_share_links");
-	                submit_button(); 
-	            ?>          
-	        </form>
-        </div>
+        <input type="checkbox" name="mbs_email_share"  value="1" <?php checked( 1, get_option( 'mbs_email_share' ), true ); ?> />
     <?php
 }
 
-add_action("admin_init", "display_social_links_fields");
-function display_social_links_fields(){
-
-    add_settings_section("section", "Social Media share:", null, "social_share_links");
-
-    add_settings_field("email_share", "Email:", "display_email_field", "social_share_links", "section");
-    add_settings_field("facebook_share", "Facebook:", "display_facebook_field", "social_share_links", "section");   
-    add_settings_field("twitter_share", "Twitter:", "display_twitter_field", "social_share_links", "section");
-    add_settings_field("instagram_share", "Instagram:", "display_instagram_field", "social_share_links", "section");
-    add_settings_field("linkedin_share", "Linkedin:", "display_linkedin_field", "social_share_links", "section");
-    
-    register_setting("section", "email_share");
-    register_setting("section", "facebook_share");
-    register_setting("section", "twitter_share");
-    register_setting("section", "instagram_share");
-    register_setting("section", "linkedin_share");
-}
-
-//Adding Social sharing checkboxes
-function display_email_field(){
+function mbs_display_twitter_field(){
     ?>
-        <input type="checkbox" name="email_share"  value="1" <?php checked(1, get_option('email_share'), true); ?> />
+        <input type="checkbox" name="mbs_twitter_share"  value="1" <?php checked( 1, get_option( 'mbs_twitter_share' ), true ); ?> />
     <?php
 }
 
-function display_twitter_field(){
+function mbs_display_facebook_field(){
     ?>
-        <input type="checkbox" name="twitter_share"  value="1" <?php checked(1, get_option('twitter_share'), true); ?> />
+        <input type="checkbox" name="mbs_facebook_share"  value="1" <?php checked( 1, get_option( 'mbs_facebook_share' ), true ); ?> />
     <?php
 }
 
-function display_facebook_field(){
+function mbs_display_pinterest_field(){
     ?>
-        <input type="checkbox" name="facebook_share"  value="1" <?php checked(1, get_option('facebook_share'), true); ?> />
+        <input type="checkbox" name="mbs_pinterest_share"  value="1" <?php checked(1, get_option( 'mbs_pinterest_share' ), true ); ?> />
     <?php
 }
 
-function display_instagram_field(){
+function mbs_display_linkedin_field(){
     ?>
-        <input type="checkbox" name="instagram_share"  value="1" <?php checked(1, get_option('instagram_share'), true); ?> />
+        <input type="checkbox" name="mbs_linkedin_share"  value="1" <?php checked( 1, get_option( 'mbs_linkedin_share' ), true ); ?> />
     <?php
 }
-
-function display_linkedin_field(){
-    ?>
-        <input type="checkbox" name="linkedin_share"  value="1" <?php checked(1, get_option('linkedin_share'), true); ?> />
-    <?php
-}
-
-
